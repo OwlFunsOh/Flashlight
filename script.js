99b226b4a4c120a095b8be2fb6b918f72a7d6050
Alfonso Martinez <amartin6@umbc.edu>
2024-11-18 21:36:43 -0500

    testing grid and flashlight

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const WALL = '■';
+const SPACE = ' '
+const START = '○'
+const board_layout = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 2, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
 const dark = document.querySelector('.dark');
 const light = document.querySelector('.light');
 
@@ -8,3 +24,24 @@ document.addEventListener('mousemove', (e) => {
     light.style.left = (pageX - 100) + 'px';
     light.style.top = (pageY - 100) + 'px';
 })
+
+
+for (let i = 0; i < board_layout.length; i++) {
+    for (let j = 0; j < board_layout[i].length; j++){
+        const newDiv = document.createElement('div');
+        newDiv.style.height = '50px';
+        newDiv.style.width = '50px';
+        newDiv.style.fontSize = '100px';
+        newDiv.style.textAlign = 'center'
+
+        if (board_layout[i][j] == 1) {
+            newDiv.textContent = WALL;
+        }else if (board_layout[i][j] == 0){
+            newDiv.textContent = SPACE;
+        }else{
+            newDiv.textContent = START;
+        }
+        mazeContainer.appendChild(newDiv);
+    }
+}
+        
